0327305bd6e8648313524911485dcd4c1fb5eb79
Oleg Per <poltava28704@gmail.com>
2019-05-28 11:38:59 +0300

    Create migration create_employees_table

---
+++ database/migrations/2019_05_28_080512_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('First name');
+            $table->string('last name');
+            $table->unsignedBigInteger('Company');
+            $table->foreign('Company')->references('id')->on('companies')->onDelete('cascade');
+            $table->string('email');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
